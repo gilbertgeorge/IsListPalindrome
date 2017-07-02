8b3ed875a3410efea1dc5c5c243c3572a8ca4036
george <george@grgilbert.com>
2017-07-02 10:08:29 -0400

    trying generic

--- IsListPalindrome/ListNode.cs
+++ IsListPalindrome/ListNode.cs
@@ -1,16 +1,43 @@
-﻿namespace ListNode
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListNode
 {
-	class ListNode<T>
+	public class ListNode<T>
 	{
 		public ListNode()
 		{
 			this.value = default(T);
 			this.next = null;
 		}
+
 		public ListNode(T cValue, ListNode<T> cNext)
 		{
 			this.value = cValue;
 			this.next = cNext;
+		}
+
+		public static bool operator ==(ListNode<T> a, T b)
+		{
+			dynamic da = a, db = b;
+			return da.value == db;
+		}
+
+		public static bool operator !=(ListNode<T> a, T b)
+		{
+			dynamic da = a, db = b;
+			return da.value == db;
+		}
+
+		public override bool Equals(object obj)
+		{
+			dynamic da = this.value, db = obj;
+			return da == db;
+		}
+
+		public override int GetHashCode()
+		{
+			return 0;
 		}
 
 		public T value { get; set; }
--- IsListPalindrome/Program.cs
+++ IsListPalindrome/Program.cs
@@ -1,42 +1,64 @@
 ﻿using ListNode;
 using System;
+using System.Collections.Generic;
 
 namespace IsListPalindrome
 {
-	public static class Program
+	public static class IsListPalindrome
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
 			ListNode<int> l = new ListNode<int>();
 			l.value = 1;
-			l.next = new ListNode<int>(2, new ListNode<int>(1,null));
+			l.next = new ListNode<int>(2, new ListNode<int>(4, new ListNode<int>(6, new ListNode<int>(4, new ListNode<int>(2, new ListNode<int>(1, null))))));
 
-			Console.Write(isListPalindrome(l));
+			Console.Write(isListPalindrome(ref l));
 
 			Console.Read();
 		}
 
-		static bool isListPalindrome(ListNode<int> l)
+		private static bool isListPalindrome<T>(ref ListNode<T> l) where T : struct
 		{
-			ListNode<int> temp = l;
-			int count = 0;
+			bool retVal = false;
+			Stack<T> values = new Stack<T>();
+			ListNode<T> t = new ListNode<T>();
+			t = l;
 
 			while (l != null)
 			{
-				Console.WriteLine(l.value);
-				count++;
-
 				if (l.next != null)
 				{
+					values.Push(l.value);
 					l = l.next;
 				}
 				else
 				{
-					break;
+					int count = values.Count;
+					while (values.Count > count / 2)
+					{
+						values.Pop();
+					}
+
+					for(int v = 0;v < values.Count;v++)
+					{
+						Console.Write(t.value);
+
+						if (t == values.Peek())
+						{
+							t = t.next;
+							values.Pop();
+						}
+						else
+						{
+							break;
+						}
+					}
+
+					retVal = true;
 				}
 			}
 
-			return false;
+			return retVal;
 		}
 
 	}
