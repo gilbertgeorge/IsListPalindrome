9cf5b4dc615d5e055e108dc614ab44b6dfa90d11
george <george@grgilbert.com>
2017-07-02 08:17:27 -0400

    fixed broken code - still doesnt work properly

---
+++ IsListPalindrome/ListNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ListNode
+{
+	class ListNode<T>
+	{
+		public ListNode()
+		{
+			this.value = default(T);
+			this.next = null;
+		}
+		public ListNode(T cValue, ListNode<T> cNext)
+		{
+			this.value = cValue;
+			this.next = cNext;
+		}
+
+		public T value { get; set; }
+		public ListNode<T> next { get; set; }
+	}
+}
--- IsListPalindrome/Program.cs
+++ IsListPalindrome/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ListNode;
+using System;
 
 namespace IsListPalindrome
 {
-	class Program
+	public static class Program
 	{
 		static void Main(string[] args)
 		{
+			ListNode<int> l = new ListNode<int>();
+			l.value = 1;
+			l.next = new ListNode<int>(2, new ListNode<int>(1,null));
+
+			Console.Write(isListPalindrome(l));
+
+			Console.Read();
 		}
+
+		static bool isListPalindrome(ListNode<int> l)
+		{
+			ListNode<int> temp = l;
+			int count = 0;
+
+			while (l != null)
+			{
+				Console.WriteLine(l.value);
+				count++;
+
+				if (l.next != null)
+				{
+					l = l.next;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			return false;
+		}
+
 	}
 }
